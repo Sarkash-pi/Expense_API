6bd27eec164bcca793d37cc15f90af1c3564ae21
Sarkash Talib <Sarkash.talib@gmail.com>
2021-10-22 00:07:14 +0100

    Created expense-list-create

--- expense_api/views.py
+++ expense_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+# import rest_framework
+# from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import ExpenseSerializer
+
+
+class ExpenseListCreateView(APIView):
+    def post(self, request):
+        serializer = ExpenseSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
